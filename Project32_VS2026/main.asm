ad94d71a302cdaa2ca2737364d2d65fbf3c1b92b
zreese2023 <zreese2023@my.fit.edu>
2026-03-31 14:30:10 -0400

    added necessary variables (subject to change). added string messages for the output. Also created skeleton value procedure for card value for face cards/ace. Additionally, made a procedure to draw a card, basically a pseudorandom number generator for numbers 1-13.

--- Project32_VS2026/main.asm
+++ Project32_VS2026/main.asm
@@ -6,7 +6,41 @@ ExitProcess PROTO, dwExitCode : DWORD
 INCLUDE irvine32.inc
 
 .data
+
+; player and dealer scores
+playerCount DWORD 0
+playerScore DWORD 0
+dealerCount DWORD 0
+dealerScore DWORD 0
+
+; cards for player and dealer
+; Note: usually in blackjack, the player
+; and the dealer will not have more than 5 cards each
+pCardArray DWORD 0,0,0,0,0
+dCardArray DWORD 0,0,0,0,0
+
+; string messages to help with output and gameplay
+playerTurnMessage BYTE "Your turn: ",0 ; player turn
+playerActionMSG BYTE "Hit or stand (h/s)?: ",0 ; player can hit or stand
+dealerTurnMessage BYTE "Dealer's turn:",0 ; dealers turn
+winMessage BYTE "You win!",0 ; player wins
+loseMessage BYTE "Dealer wins :(",0 ; player loses, dealer wins
+pushMessage BYTE "Push (draw)",0 ; push (draw)
+
 .code
+
+Draw PROC
+	; get a random card procedure (1-13)
+	mov eax,13 ; 13 possible card values
+	call RandomRange ; irvine32 procedure, puts a random integer into eax from range 0 to eax-1
+	inc eax ; 0 isnt a possible value, so add 1 to eax so we have values 1-13
+	mov ecx,eax ; move result to ecx use in determining value (if ace or face card)
+	ret
+Draw ENDP
+
+Value PROC
+	; handle face cards and ace value
+Value ENDP
 
 DrawP PROC
 ; Player drawing cards procedure
@@ -40,10 +74,10 @@ Score ENDP
 
 main PROC
 	call Randomize ; irvine32 procedure to generate a new seed for a random number generator
-	call DrawPCards ; get first 2 player cards
-	call DrawPCards
-	call DrawDCards ; get first 2 dealer cards
-	call DrawDCards
+	call DrawP ; get first 2 player cards
+	call DrawP
+	call DrawD ; get first 2 dealer cards
+	call DrawD
 	call PTurn ; player turn
 	call DTurn ; dealer turn
 	call ShowDCards ; show dealer cards using procedure
