7497789262b853cbb584601082e04bf85b5f329e
zreese2023 <zreese2023@my.fit.edu>
2026-03-31 13:30:55 -0400

    added all of the basic necessary procedures (not yet coded) for the blackjack game. These include the player and dealer drawing, showing the cards, taking turns, and determining the winner.

--- Project32_VS2026/main.asm
+++ Project32_VS2026/main.asm
@@ -1,11 +1,43 @@
 .386
 .model flat, stdcall
-ExitProcess PROTO,0
+.stack 4096
+ExitProcess PROTO, dwExitCode : DWORD
 
 INCLUDE irvine32.inc
 
 .data
 .code
+
+DrawP PROC
+; Player drawing cards procedure
+DrawP ENDP
+
+DrawD PROC
+; Draw dealer cards procedure
+DrawD ENDP
+
+ShowPCards PROC
+; show player cards procedure
+ShowPCards ENDP
+
+ShowDCards PROC
+; show dealer cards procedure
+ShowDCards ENDP
+
+PTurn PROC
+; main procedure for player turn
+PTurn ENDP
+
+DTurn PROC
+; procedure for dealer turn
+; dealer will stand on soft 17
+; for this case
+DTurn ENDP
+
+Score PROC
+; determine winner: player or dealer
+Score ENDP
+
 main PROC
 
 main ENDP
